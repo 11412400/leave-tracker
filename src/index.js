b115da512c45f497ae8cd962f13ae317e94f08ad
11412400 <changyeonkim1106@gmail.com>
2025-05-06 14:50:44 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import LeaveTracker from './LeaveTracker';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <LeaveTracker />
-  </React.StrictMode>
-);
